29357518d859821be1f6447060d998d3ba371376
slimreaperx <chammonds81@gmail.com>
2023-12-12 15:17:45 -0600

    New changes

---
+++ index.js
@@ -0,0 +1,10 @@
+const freelancers = [
+    { name: "Iron Man", price: 25, occupation: "Mechanic" },
+    { name: "Hulk", price: 51, occupation: "Therapist" },
+    { name: "Captain America", price: 43, occupation: "Security Guard" },
+    { name: "Spider Man", price: 81, occupation: "Science Professor" },
+    { name: "Wanda Maximoff", price: 43, occupation: "Pyschiatrist" },
+    { name: "Black Panther", price: 76, occupation: "Humanitarian" },
+    { name: "Vision", price: 47, occupation: "teacher" },
+    { name: "Wolverine", price: 72, occupation: "driver" },
+  ];
